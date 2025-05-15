338c02d25b10dbfdebe1c75f4833ba2ad4eaf88e
Adrien <115026138+adrien-pago@users.noreply.github.com>
2025-05-15 10:03:29 +0200

    up

--- vaca-meet-api/src/Controller/TestController.php
+++ vaca-meet-api/src/Controller/TestController.php
@@ -24,4 +24,16 @@ class TestController extends AbstractController
             ]
         ]);
     }
-} 
+    
+    #[Route('/api/test', name: 'api_test', methods: ['GET'])]
+    public function test(): JsonResponse
+    {
+        return $this->json([
+            'status' => 'success',
+            'message' => 'API fonctionne correctement!',
+            'timestamp' => new \DateTime(),
+            'environment' => $_ENV['APP_ENV'] ?? 'unknown',
+            'app' => 'Vaca Meet Mobile API'
+        ]);
+    }
+}
